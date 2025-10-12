448ca65633d661b732b5f89e658be3e7ee06e917
malikcancode <malikoffical32@gmail.com>
2025-10-12 10:57:05 +0500

    updated backend cors

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ connectDB();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://www.partnersellercentre.shop", // <-- REMOVE trailing slash
+    "https://pec-app-frontend.vercel.app/",
+    // "https://www.partnersellercentre.shop", // <-- REMOVE trailing slash
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
